a82e8cbaa8fa88a2b9afba9ab8fe14f7a7bf159f
Matheus Grzibovski Vieira <matheusgvieira141@gmail.com>
2023-04-09 13:19:49 -0400

    versão mobile/x items left/show all

--- toDoController.js
+++ toDoController.js
@@ -1,22 +1,53 @@
 let listaTodos =  JSON.parse(localStorage.getItem("salvarToDo")) || []
-
+let listCompleted = []
 class Controller{
     constructor(){
         this.container = document.querySelector('#container');
         
-  
+        
         this.render();
         
-        document.querySelector('#addTodo').addEventListener('click' , (e) => {
+        /* document.querySelector('#addTodo').addEventListener('click' , (e) => {
             e.preventDefault();
             this.add();
-            this.clearInput();
+            ;
+        }); */   
+        
+        document.querySelector('#clearCompleted').addEventListener('click' , (e) => {
+            this.clearCompleted()
         });   
         
-       
+        document.querySelector('#showCompleted').addEventListener('click' , (e) => {
+            this.showCompleted()
+        });
+        
+        document.querySelector('#showActive').addEventListener('click' , (e) => {
+            this.showActive()
+        }); 
+        document.querySelector('#all').addEventListener('click' , (e) => {
+            this.render()
+        }); 
+        document.querySelector("#toDOInput").addEventListener('keydown', (e)=>{
+            if (e.key === 'Enter') {
+                this.add();
+            }
+        });
+        
     } 
-  
-
+    
+    qtd(){
+        this.Qtd = 0 
+        for(let i =0 ;i<listaTodos.length;i++){
+            
+            if(listaTodos[i].done==false){
+                this.Qtd++; 
+                
+            }
+        };
+        
+        document.querySelector('#qtd').innerHTML = this.Qtd+" items left";
+        
+    }
     render(){
         this.container.innerHTML = '';
         listaTodos.forEach((todo, index) => {
@@ -24,6 +55,7 @@ class Controller{
         });
         
         this.bind();
+        this.qtd()
     }
     bind(){
         document.querySelectorAll('.btnDelete').forEach((btn) => {
@@ -45,28 +77,80 @@ class Controller{
         listaTodos.push(new toDoModel(todo , false));
         this.render();
         this.salvarToDo()
-       
+        this.clearInput()
         
     }
     delete(targetButton){
         let index = targetButton.closest('div').dataset.index;
         targetButton.closest('div').remove();
-   
+        
         listaTodos.splice(index ,1);
         this.render();
         this.salvarToDo()
+        
     }
     check(targetButton){
         let index = targetButton.closest('div').dataset.index;
         listaTodos[index].done = !(listaTodos[index].done);
         this.render(); 
-        this.salvarToDo()
+        this.salvarToDo() ;
+        
     }
     salvarToDo(){
-       
+        this.qtd()
         localStorage.setItem('salvarToDo' , JSON.stringify(listaTodos))
         
     }
+    
+    clearCompleted(){
+        
+        for(let i =0;i<listaTodos.length ;i++) {
+            if(listaTodos[i].done == true){
+                listaTodos.splice(i ,1);
+                this.clearCompleted()
+            }
+        }
+        this.salvarToDo()
+        this.render()
+        
+    }
+    showCompleted(){
+        listCompleted = []
+        this.container.innerHTML = ''
+        
+        for(let i =0;i<listaTodos.length ;i++) {
+            if(listaTodos[i].done == true){
+                listCompleted.push(listaTodos[i])
+            }
+        }
+        
+        this.container.innerHTML = '';
+        listCompleted.forEach((todo, index) => {
+            this.container.innerHTML += new TodoView(todo, index).template();
+        });
+        
+        this.bind();
+    }
+    showActive(){
+        listCompleted = []
+        this.container.innerHTML = ''
+        
+        for(let i =0;i<listaTodos.length ;i++) {
+            if(listaTodos[i].done == false){
+                listCompleted.push(listaTodos[i])
+            }
+        }
+        
+        this.container.innerHTML = '';
+        listCompleted.forEach((todo, index) => {
+            this.container.innerHTML += new TodoView(todo, index).template();
+        });
+        this.qtd()
+        this.bind();
+    }
+    
+    
+    
     clearInput() {
         document.querySelector('#toDOInput').value = '';
     }
